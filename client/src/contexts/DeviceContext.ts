62f18ed308a724db8eaf735233b0ba4455fd96c5
Artem Shchirov <artemshchirov@gmail.com>
2023-01-03 19:10:18 +0200

    refactor(client): add UserContext and DeviceContext

---
+++ client/src/contexts/DeviceContext.ts
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+import DeviceStore from "../store/DeviceStore";
+
+export interface IDeviceContext {
+  device?: DeviceStore;
+}
+
+export const DeviceContext = createContext<IDeviceContext>({});
---
+++ client/src/contexts/UserContext.ts
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+import UserStore from "../store/UserStore";
+
+export interface IUserContext {
+  user?: UserStore;
+}
+export const UserContext = createContext<IUserContext>({});
