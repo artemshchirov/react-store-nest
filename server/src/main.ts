ffad564f7d5e5b730d7eacbc66639bacb56cf2e5
Artem Schirov <artemschirov@gmail.com>
2023-01-08 21:43:07 +0200

    feat(server): add postgres sequelize module

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+// NOTE: nodemon?
+async function start() {
+  const PORT = process.env.PORT || 5000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
-bootstrap();
+
+start();
