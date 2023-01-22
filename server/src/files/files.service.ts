d3442769c2e5d8b0c07ab7d42ca27e6005b2f2e9
Artem Shchirov <artemshchirov@gmail.com>
2023-01-22 18:26:11 +0200

    feat(server): add /files module and share static images

---
+++ server/src/files/files.service.ts
@@ -0,0 +1,26 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { v4 } from 'uuid';
+import * as path from 'path';
+import * as fs from 'fs';
+
+@Injectable()
+export class FilesService {
+  // TODO: async work with filesystem
+  async createFile(file): Promise<string> {
+    try {
+      const fileName = v4() + '.jpg';
+      const filePath = path.resolve(__dirname, '..', 'static');
+
+      if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath, { recursive: true });
+      }
+      fs.writeFileSync(path.join(filePath, fileName), file.buffer);
+      return fileName;
+    } catch (err) {
+      throw new HttpException(
+        'Error while saving file to disk',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
